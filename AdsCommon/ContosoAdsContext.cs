401fc4740994bb4507a3bb33097a5c826a0d7ec4
alexander grajales <alex-12-04@hotmail.com>
2016-09-23 05:45:02 -0500

    conf

---
+++ AdsCommon/ContosoAdsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace AdsCommon
+{
+    public class ContosoAdsContext : DbContext
+    {
+        public ContosoAdsContext() : base("ContosoAdsContext")
+        {
+        }
+
+        public ContosoAdsContext(string connString) : base(connString)
+        {
+        }
+
+        public System.Data.Entity.DbSet<Ad> Ads { get; set; }
+    }
+}
